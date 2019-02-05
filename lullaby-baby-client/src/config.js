d501acefee3325d2370f20e7396e79cc02456032
Chris Ranni <admin@marx.tech>
2019-02-05 16:28:03 -0500

    fix config

--- lullaby-baby-client/src/config.js
+++ lullaby-baby-client/src/config.js
@@ -3,16 +3,16 @@ export default {
   STRIPE_KEY: "pk_test_3ZzXQnkXHshCiViaxXuhqTr9",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "https://s3.console.aws.amazon.com/s3/buckets/lullaby-baby-v2-v2-attachmentsbucket-12bdy8yzugyuz/?region=us-east-1&tab=overview"
+    BUCKET: "lullaby-baby-v2-v2-attachmentsbucket-12bdy8yzugyuz"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://gsfvzxgf61.execute-api.us-east-1.amazonaws.com/v1"
+    URL: "https://seqvx47i08.execute-api.us-east-1.amazonaws.com/v2"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yiGyURiS7",
-    APP_CLIENT_ID: "7uvnlrjb6ebn3ei1hdrc6u8lvl",
-    IDENTITY_POOL_ID: "us-east-1:7689a92d-bebf-46c0-bc8f-16773ea771d4"
+    USER_POOL_ID: "us-east-1_tfn9thIKg",
+    APP_CLIENT_ID: "6jabip9uv2vp32pbfskhaire59",
+    IDENTITY_POOL_ID: "us-east-1:f26b6502-b0b4-4618-a893-38e6cc97a31b"
   }
 };
